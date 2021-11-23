a28b590019e58b68a78960bcbb9282252b95b7c8
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-11-22 20:00:44 -0500

    redux boilerplate setup

---
+++ src/redux/reducer.js
@@ -0,0 +1,16 @@
+import {
+  ACTION_TYPE
+} from './actionTypes';
+
+const initialState = {
+ state_here: 0
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
